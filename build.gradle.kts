2122af6e3cb0653ecf529be3966c1e7fee1a7b92
AnryAnryK <andkay@yandex.ru>
2025-02-03 12:24:41 +0300

    v repositoriy

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java")
+    id("io.qameta.allure") version "2.12.0" //для Аллюр- отчётов
+}
+
+group = "AnryAnryK.com.GitHub"
+version = "1.0-SNAPSHOT"
+
+allure {
+    report {
+        version.set("2.19.0")
+    }
+    adapter { // отвечает за появление папки build/allure-results
+        aspectjWeaver.set(true) //обработка аннотации @Step
+        frameworks {
+            junit5 { //название фреймворка
+                adapterVersion.set("2.19.0") //версия интеграции фреймворка и Allure
+            }
+        }
+    }
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1") //JUnit5  - !!! это АГРЕГАТОР (он включает в себя 1-ю и 2-ю минимально необходимые зависимости, т.е. можно ТОЛЬКО ЕГО ИСПОЛЬЗОВАТЬ !!!)
+    testImplementation("com.codeborne:selenide:7.5.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0") //для Аллюр- отчётов
+    testImplementation("io.qameta.allure:allure-selenide:2.19.0") //для Аллюр- отчётов
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0") //для Аллюр- отчётов
+    testImplementation("io.rest-assured:rest-assured:5.5.0") //для тестирования Rest Api
+    testImplementation("com.github.javafaker:javafaker:1.0.2")  //javaFaker
+}
+
+tasks.test {
+    useJUnitPlatform()
+//    systemProperties(System.getProperties()) //для Аллюр- отчётов
+}
---
+++ src/test/java/SaucedemoComTests/SaucedemoComAutorization.java
@@ -0,0 +1,47 @@
+package SaucedemoComTests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class SaucedemoComAutorization {
+
+	@Test
+	@Description("Авторизоваться на сайте")
+	@Owner("Калинченко Андрей")
+
+
+	public void SaucedemoComAutorizationPositive() {
+
+		Configuration.pageLoadStrategy = "eager";
+		Configuration.browserSize = "1920x1080";
+		SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+		step("1 Шаг: Зайти на сайт https://www.saucedemo.com/", () ->
+		{
+			open("https://www.saucedemo.com/");
+		});
+		step("2 Шаг: Ввести Username", () ->
+		{
+			$x(".//*[@id='user-name']").setValue("standard_user");
+		});
+		step("3 Шаг: ввести Password", () ->
+		{
+			$x(".//*[@id='password']").setValue("secret_sauce");
+		});
+		step("4 Шаг: нажать кнопку Login", () ->
+		{
+			$x(".//*[@id='login-button']").click();
+		});
+		step("5 Шаг: убрать всплывающее окно (кликнуть в свободное место)", () ->
+		{
+			$x(".//*[@class='page_wrapper']").click();
+		});
+	}
+}
